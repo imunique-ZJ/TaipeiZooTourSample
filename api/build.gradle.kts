5c6cf47178192f96cd17760d914787beacb714bc
Z.J <imunique.zj@gmail.com>
2020-07-27 22:33:40 +0800

    @Purpose [api module] add required libraries - jackson-csv and coroutines

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -28,11 +28,13 @@ android {
 dependencies {
     implementation(fileTree( mapOf("dir" to "libs", "include" to listOf("*.jar")) ))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${ProjectConfig.kotlinVersion}")
-    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7")
+    implementation("androidx.core:core-ktx:1.3.0")
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.11.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.1")
 
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
