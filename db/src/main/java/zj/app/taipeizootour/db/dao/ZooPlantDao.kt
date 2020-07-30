351a4ceb24e57cf0351da5265556d4137b1b923b
Z.J <imunique.zj@gmail.com>
2020-07-30 08:31:38 +0800

    @Purpose [api module] add ZooPlantDao with simple CRUD

---
+++ db/src/main/java/zj/app/taipeizootour/db/dao/ZooPlantDao.kt
@@ -0,0 +1,21 @@
+package zj.app.taipeizootour.db.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import zj.app.taipeizootour.db.model.ZooPlant
+
+@Dao
+interface ZooPlantDao {
+
+    @Query("SELECT * FROM ZooPlant ORDER BY id ASC")
+    fun queryAll(): LiveData<List<ZooPlant>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(zooPlants: List<ZooPlant>)
+
+    @Query("DELETE FROM ZooPlant")
+    suspend fun deleteAll()
+}
