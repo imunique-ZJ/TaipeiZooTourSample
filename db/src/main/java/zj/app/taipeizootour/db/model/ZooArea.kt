8adb1c2ca042a2333092b84c4e47022397dffac8
Z.J <imunique.zj@gmail.com>
2020-07-30 08:09:49 +0800

    @Purpose [api module] add ZooArea entity

---
+++ db/src/main/java/zj/app/taipeizootour/db/model/ZooArea.kt
@@ -0,0 +1,22 @@
+package zj.app.taipeizootour.db.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ZooArea(
+    @PrimaryKey
+    val id: String,
+    val title: String,
+    val type: String,
+    val category: String,
+    val metadataModified: String,
+    val issued: String,
+    val numberOfData: Int,
+    val orgId: String,
+    val orgName: String,
+    val organizationName: String,
+    val subOrgId: String,
+    val subOrgName: String,
+    val tag: String
+)
