726b1c459e2c9c015c9ab477a43c6dcf21594e58
Z.J <imunique.zj@gmail.com>
2020-08-01 11:35:06 +0800

    @Purpose [app module] init main activity layout design

--- app/src/main/java/zj/app/taipeizootour/MainActivity.kt
+++ app/src/main/java/zj/app/taipeizootour/MainActivity.kt
@@ -1,11 +1,17 @@
 package zj.app.taipeizootour
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import zj.app.taipeizootour.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
     }
+
 }
