015d4cbeb569d48e7408e446e94592dd84279aa3
Z.J <imunique.zj@gmail.com>
2020-08-01 10:44:57 +0800

    @Purpose [db module] add @Transaction annotation to query functions of AreaPlantsDao to ensure whole operation is performed atomically @Ref https://developer.android.com/training/data-storage/room/relationships#many-to-many

--- db/src/main/java/zj/app/taipeizootour/db/dao/AreaPlantsDao.kt
+++ db/src/main/java/zj/app/taipeizootour/db/dao/AreaPlantsDao.kt
@@ -1,19 +1,18 @@
 package zj.app.taipeizootour.db.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import zj.app.taipeizootour.db.data.AreaWithPlants
 import zj.app.taipeizootour.db.model.AreaPlantsCrossRef
 
 @Dao
 interface AreaPlantsDao {
 
+    @Transaction
     @Query("SELECT * FROM ZooArea")
     fun queryAll(): LiveData<List<AreaWithPlants>>
 
+    @Transaction
     @Query("SELECT * FROM ZooArea WHERE areaId = :areaId")
     fun queryByArea(areaId: Int): LiveData<List<AreaWithPlants>>
 
