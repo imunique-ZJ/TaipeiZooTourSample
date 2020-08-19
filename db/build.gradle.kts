37e13b5f853a7da8e82a9666e7baa5a02e467558
Z.J <imunique.zj@gmail.com>
2020-08-19 12:06:03 +0800

    @Purpose db module doesn't need hilt dependencies

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
+    kotlin("android")
+    kotlin("android.extensions")
     kotlin("kapt")
 }
 
@@ -44,11 +44,7 @@ dependencies {
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:${Dependency.roomVersion}")
 
-    implementation("com.google.dagger:hilt-android:2.28-alpha")
-    kapt("com.google.dagger:hilt-android-compiler:2.28-alpha")
-
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
-
 }
